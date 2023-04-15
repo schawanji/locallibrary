83730461fd049208117f2c9c6b0c9b86ec588a39
schawanji <schawanji@gmail.com>
2023-04-15 16:14:39 +0200

    Add sessions to improve interactions with anonymous users

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -4,15 +4,19 @@ from . import models
 
 # Create your views here.
 def home(request):
-    num_books = models.Book.objects.all().count()
+    num_books = models.Book.objects.all().count() # num_books = models.Book.objects.count() is also ok all() is implied by default
     num_instances=models.BookInstance.objects.all().count()
     num_instances_available=models.BookInstance.objects.filter(status='a').count()
     num_authors = models.Author.objects.count()
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
     context={
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_visits':num_visits,
     }
     return render(request,'index.html',context=context)
 
